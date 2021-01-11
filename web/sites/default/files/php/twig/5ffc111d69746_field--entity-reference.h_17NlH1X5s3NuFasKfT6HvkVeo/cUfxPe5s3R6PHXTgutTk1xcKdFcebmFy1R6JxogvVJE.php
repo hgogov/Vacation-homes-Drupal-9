bd3aee1e71dbebe0c2b487cedf10e9bb2aab9a0f
Hristian Gogov <hristian098@abv.bg>
2021-01-11 11:47:59 +0200

    changed drupal theme

---
+++ web/sites/default/files/php/twig/5ffc111d69746_field--entity-reference.h_17NlH1X5s3NuFasKfT6HvkVeo/cUfxPe5s3R6PHXTgutTk1xcKdFcebmFy1R6JxogvVJE.php
@@ -0,0 +1,184 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/magazine_lite/templates/field--entity-reference.html.twig */
+class __TwigTemplate_a96c2fe480b4083b93a19923c2a6c0d00947e9fa32f18212f3d1c337bb363d0d extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array("set" => 25, "if" => 40, "for" => 43);
+        $filters = array("clean_class" => 27, "escape" => 42);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'for'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 25
+        $context["classes"] = [0 => "field", 1 => ("field--name-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 27
+($context["field_name"] ?? null), 27, $this->source))), 2 => ("field--type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 28
+($context["field_type"] ?? null), 28, $this->source))), 3 => ("field--label-" . $this->sandbox->ensureToStringAllowed(        // line 29
+($context["label_display"] ?? null), 29, $this->source)), 4 => ("field--entity-reference-target-type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 30
+($context["entity_reference_target_type"] ?? null), 30, $this->source))), 5 => "clearfix"];
+        // line 35
+        $context["title_classes"] = [0 => "field__label", 1 => (((        // line 37
+($context["label_display"] ?? null) == "inline")) ? ("inline") : (""))];
+        // line 40
+        if (($context["label_hidden"] ?? null)) {
+            // line 41
+            echo "  ";
+            if (($context["multiple"] ?? null)) {
+                // line 42
+                echo "    <div";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null), 1 => "field__items"], "method", false, false, true, 42), 42, $this->source), "html", null, true);
+                echo ">
+      ";
+                // line 43
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 44
+                    echo "        <div";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 44), "addClass", [0 => "field__item"], "method", false, false, true, 44), 44, $this->source), "html", null, true);
+                    echo ">";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 44), 44, $this->source), "html", null, true);
+                    echo "</div>
+      ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 46
+                echo "    </div>
+  ";
+            } else {
+                // line 48
+                echo "    ";
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 49
+                    echo "      <div";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null), 1 => "field__item"], "method", false, false, true, 49), 49, $this->source), "html", null, true);
+                    echo ">";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 49), 49, $this->source), "html", null, true);
+                    echo "</div>
+    ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 51
+                echo "  ";
+            }
+        } else {
+            // line 53
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 53), 53, $this->source), "html", null, true);
+            echo ">
+    <div";
+            // line 54
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", [0 => ($context["title_classes"] ?? null)], "method", false, false, true, 54), 54, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 54, $this->source), "html", null, true);
+            echo "</div>
+    ";
+            // line 55
+            if (($context["multiple"] ?? null)) {
+                // line 56
+                echo "      <div class=\"field__items\">
+    ";
+            }
+            // line 58
+            echo "    ";
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+            foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                // line 59
+                echo "      <div";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 59), "addClass", [0 => "field__item"], "method", false, false, true, 59), 59, $this->source), "html", null, true);
+                echo ">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 59), 59, $this->source), "html", null, true);
+                echo "</div>
+    ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 61
+            echo "    ";
+            if (($context["multiple"] ?? null)) {
+                // line 62
+                echo "      </div>
+    ";
+            }
+            // line 64
+            echo "  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/magazine_lite/templates/field--entity-reference.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  160 => 64,  156 => 62,  153 => 61,  142 => 59,  137 => 58,  133 => 56,  131 => 55,  125 => 54,  120 => 53,  116 => 51,  105 => 49,  100 => 48,  96 => 46,  85 => 44,  81 => 43,  76 => 42,  73 => 41,  71 => 40,  69 => 37,  68 => 35,  66 => 30,  65 => 29,  64 => 28,  63 => 27,  62 => 25,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/magazine_lite/templates/field--entity-reference.html.twig", "C:\\xampp\\htdocs\\drupal\\web\\themes\\contrib\\magazine_lite\\templates\\field--entity-reference.html.twig");
+    }
+}
